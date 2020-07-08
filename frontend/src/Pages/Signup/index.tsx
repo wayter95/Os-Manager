900f089bd0e9a5b7927404f61099766970aea344
WalberFellipe <walberfellipe97@gmail.com>
2020-07-07 22:44:41 -0300

    Estilização Signup

--- frontend/src/Pages/Signup/index.tsx
+++ frontend/src/Pages/Signup/index.tsx
@@ -6,14 +6,15 @@ const Signup = () => {
     return (
         <div className="page-signup">
             <div className="content">
-                <h1>REGISTRE-SE</h1>
+                <h1>Registre-se</h1>
+                <form>
 
                 <label htmlFor="name">Nome: *</label>
-                <input type="name"
+                <input type="text"
                     placeholder="Digite seu nome:" />
 
                 <label htmlFor="username">Nome de Usuario: *</label>
-                <input type="UserName"
+                <input type="text"
                     placeholder="Digite um nome de usuario:" />
 
 
@@ -23,12 +24,13 @@ const Signup = () => {
 
 
                 <label htmlFor="nome">Código de acesso: *</label>
-                <input type="accesscode"
+                <input type="text"
                     placeholder="Digite aqui o codigo de acesso:" />
 
                 <button className="btn" type="submit">Continuar</button>
-                <a href="/signin">Caso já possua cadastro clique aqui.</a>
-                
+                <a href="/">Já possui uma conta ? Acesse aqui.</a>
+                </form>
+                <p className="footer">Created by <strong>Creative Project</strong> size in 2020</p>
             </div>
         </div>
     )
