d39f586a58a8b16f87a212d3dac99d678599136e
WalberFellipe <walberfellipe97@gmail.com>
2020-07-08 23:22:19 -0300

    Correcao Botao Continue e Criacao select cargo

--- frontend/src/Pages/Signup/index.tsx
+++ frontend/src/Pages/Signup/index.tsx
@@ -1,39 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
 const Signup = () => {
-    return (
-        <div id="page-signup">
-            <div className="register">
-                <h1>Registre-se</h1>
-                <form>
+  const [display, setDisplay] = useState('register');
 
-                <label htmlFor="name">Nome: *</label>
-                <input type="text"
-                    placeholder="Digite seu nome:" />
+  return (
+    <form>
+      <div id="page-signup">
+        {display === 'register' && (
+          <div className="register">
+            <h1>Registre-se</h1>
+            <label htmlFor="name">Nome: *</label>
+            <input type="text" placeholder="Digite seu nome:" />
 
-                <label htmlFor="username">Nome de Usuario: *</label>
-                <input type="text"
-                    placeholder="Digite um nome de usuario:" />
+            <label htmlFor="username">Nome de Usuario: *</label>
+            <input type="text" placeholder="Digite um nome de usuario:" />
 
+            <label htmlFor="password">Senha: *</label>
+            <input type="password" placeholder="Digite sua senha:" />
 
-                <label htmlFor="password">Senha: *</label>
-                <input type="password"
-                    placeholder="Digite sua senha:" />
+            <label htmlFor="accessCode">Código de acesso: *</label>
+            <input type="text" name= "accessCode" placeholder="Digite aqui o codigo de acesso:" />
 
+            <button
+              className="btn"
+              type="button"
+              onClick={() => setDisplay('continue')}
+            >
+              Continuar
+            </button>
+            <h2>
+              Já possui uma conta ?<a href="/">Acesse aqui!</a>
+            </h2>
+            <p className="footer">
+              Created by <strong>Creative Project</strong> size in 2020
+            </p>
+          </div>
+        )}
+        {display === 'continue' && (
+          <div className="continue">
+            <h1>Dados de cadastro</h1>
+            <label htmlFor="email">E-mail: *</label>
+            <input type="email" placeholder="Digite seu e-mail:" />
 
-                <label htmlFor="nome">Código de acesso: *</label>
-                <input type="text"
-                    placeholder="Digite aqui o codigo de acesso:" />
+            <label htmlFor="phone">Telefone: *</label>
+            <input
+              type="text"
+              placeholder="Digite seu Telefone para contato:"
+            />
 
-                <button className="btn" type="submit">Continuar</button>
-                <a href="/">Já possui uma conta ? Acesse aqui.</a>
-                </form>
-                <p className="footer">Created by <strong>Creative Project</strong> size in 2020</p>
-            </div>
-        </div>
-    )
-}
+            <label htmlFor="sector">Setor: *</label>
+            <select name="setor" id="sector">
+              <option value="Infras">Infras</option>
+              <option value="Administrativo">Administrativo</option>
+            </select>
+
+            <label htmlFor="office">Cargo: *</label>
+            <input type="text" placeholder="Digite seu cargo:" />
+
+            <button className="btn" type="submit">
+              Concluir
+            </button>
+            <p className="footer">
+              Created by <strong>Creative Project</strong> size in 2020
+            </p>
+          </div>
+        )}
+      </div>
+    </form>
+  );
+};
 
 export default Signup;
