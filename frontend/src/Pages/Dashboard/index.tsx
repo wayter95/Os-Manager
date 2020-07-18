56e0e0518aee1a2d8b38d76f62051a769cf73fd9
wayter <wayter.paulo.95@gmail.com>
2020-07-17 21:58:37 -0300

    create page dashboard and settings

---
+++ frontend/src/Pages/Dashboard/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Header from '../../Components/Header';
+import Sidebar from '../../Components/Sidebar';
+
+import './style.css';
+
+const Dashboard = () => {
+    return (
+        <div id="page-dashboard">
+            <Header />
+            <Sidebar />
+            <div className="content">
+                <div className="osOpen">
+                    <h1>Ordens de Serviços Abertas:</h1>
+                    <span>21</span>
+                </div>
+                <div className="notifications">
+                    <h1>Notificações:</h1>
+                    <ul>
+                        <li>
+                            <p>USUARIO iniciou uma criou uma ordem de serviço</p>
+                            <span>Alta</span>
+                            <p>10/06/2020</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="graph">
+                    <h1>Ordens de Serviços Diarias:</h1>
+                    <label htmlFor="week">
+                        <input type="checkbox" />
+                        Semanal
+                    </label>
+
+                    <label htmlFor="month">
+                        <input type="checkbox" />
+                        Mensal
+                    </label>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard;
