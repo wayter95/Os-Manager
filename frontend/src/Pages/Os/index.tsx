45d9f770e477c71d6d05c704d43a0d7358622044
wayter <wayter.paulo.95@gmail.com>
2020-07-18 00:28:58 -0300

    init settings in page os

--- frontend/src/Pages/Os/index.tsx
+++ frontend/src/Pages/Os/index.tsx
@@ -5,15 +5,59 @@ import Sidebar from '../../Components/Sidebar';
 import './style.css';
 
 const Os = () => {
-return(
-    <div>
-        <Header/>
-        <Sidebar/>
-        <div>
-            <h1></h1>
+    return (
+        <div id="page-os">
+            <Header />
+            <Sidebar />
+            <div className="content">
+                <div className="createOs">
+                    <h1>Criar OS:</h1>
+                    <form action="">
+                        <label className="subject">
+                            <p>Assunto: *</p>
+                            <input type="text" />
+                        </label>
+
+                        <label className="description">
+                            <p>Descrição: *</p>
+                            <textarea />
+                        </label>
+
+                        <label className="priority">
+                            <p>Priodidade:</p>
+                            <select name="" id="">
+                                <option value="">Baixa</option>
+                                <option value="">Normal</option>
+                                <option value="">Alta</option>
+                            </select>
+                        </label>
+
+                        <label className="equipaments">
+                            <p>Equipamento: *</p>
+                            <input type="text" />
+                        </label>
+
+                        <label className="local">
+                            <p>Local: *</p>
+                            <input type="text" />
+                        </label>
+
+                        <label className="check">
+                            <input type="checkbox" />
+                        Troca de Equipamento
+                        </label>
+
+                        <button type="submit">Salvar</button>
+
+
+                    </form>
+                </div>
+                <div className="allOs">
+                    <h1>Todas as minnhas OS:</h1>
+                </div>
+            </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default Os;
