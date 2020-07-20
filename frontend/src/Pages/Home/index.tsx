8a73ac4b01317a2f74671f215bfae422e9150280
WalberFellipe <walberfellipe97@gmail.com>
2020-07-20 16:12:34 -0300

    adjusts in home style

--- frontend/src/Pages/Home/index.tsx
+++ frontend/src/Pages/Home/index.tsx
@@ -1,32 +1,41 @@
-import React from 'react';
-import {useHistory} from 'react-router-dom';
-import Header from '../../Components/Header';
-import Sidebar from '../../Components/Sidebar';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Header from "../../Components/Header";
+import Sidebar from "../../Components/Sidebar";
 
-import './style.css';
-
-    
+import "./style.css";
 
 const Home = () => {
-    const history = useHistory();
-return(
+  const history = useHistory();
+  return (
     <div id="page-home">
-        <Header/> 
-        <Sidebar/>
-        <div className="content">
-            <div className="lastOs">
-            <h1>Ultimas OS <button onClick={() => history.push('/os')}> Criar OS</button> </h1>
-            <ul>ASSUNTO DATA OS PRIORIDADE ANDAMENTO</ul>
-            <li>Formatacao 22/03/199 <strong>alta</strong> concluido</li>
-            </div>
-            
+      <Header />
+      <Sidebar />
+      <div className="content">
+        <div className="lastOs">
+          <ul>
+            <li>
+              <h1>Ultimas OS </h1>
+              <button onClick={() => history.push("/os")}> Criar OS</button>
+            </li>
+          </ul>
+          </div>
+        <div className="createOs">
+          <ul>
+            <li>
+            <b>Assunto</b>
+            <b>Data Os</b>
+            <b>Prioridade</b>
+            <b>andamento</b>
+            </li>
+          </ul>
 
-            <div className="notifications">
-            <h1>Notificações</h1>
-            </div>
-            
         </div>
-    </div>
-)
-}
+        </div>
+        <div className="notifications">
+          <h1>Notificações</h1>
+        </div>
+      </div>
+  );
+};
 export default Home;
