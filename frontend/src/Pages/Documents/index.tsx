9596a27fd4a73fc791d518939453084f86ffa92b
wayter95 <wayter.paulo.95@gmail.com>
2020-07-27 10:28:09 -0300

    Add spiner in signin button

--- frontend/src/Pages/Documents/index.tsx
+++ frontend/src/Pages/Documents/index.tsx
@@ -1,48 +1,53 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import Header from '../../Components/Header';
-import Sidebar from '../../Components/Sidebar';
+import Header from "../../Components/Header";
+import Sidebar from "../../Components/Sidebar";
 
-import Withdrawal from '../../Components/Withdrawal';
-import MainReport from '../../Components/MainReport';
-import Specifications from '../../Components/Specifications';
+import Withdrawal from "../../Components/Withdrawal";
+import MainReport from "../../Components/MainReport";
+import Specifications from "../../Components/Specifications";
 
-import './style.css';
+import "./style.css";
 
 const Documents = () => {
-    const [state,setState] = useState('w');
-        useEffect(() => {
-           
-        },[])
-    return (
-        <div id="page-documents">
-            <Header />
-            <Sidebar />
-            <div className="content">
-                <div className="buttons">
-                    <button id="w" onClick={() => setState('w')}>Retirada<br/>de equipamento</button>
-                    <button id="m" onClick={() => setState('m')}>Relatorio<br/>de manutenção</button>
-                    <button onClick={() => setState('s')}>Especificações<br/>de equipamento</button>
-                    <button className="disab" disabled onClick={() => setState('o')}>Ordem<br/>de compra</button>
-                </div>
-                <div className="form">
-                    <h1>Dados para documento:</h1>
-                   {
-                       state === 'w' &&
-                       <Withdrawal />
-                   }
-                    {
-                       state === 'm' &&
-                       <MainReport />
-                   }
-                    {
-                       state === 's' &&
-                       <Specifications />
-                   }
-                </div>
-            </div>
+  const [state, setState] = useState("w");
+
+  return (
+    <div id="page-documents">
+      <Header />
+      <Sidebar />
+      <div className="content">
+        <div className="buttons">
+          <button id="w" onClick={() => setState("w")}>
+            Retirada
+            <br />
+            de equipamento
+          </button>
+          <button id="m" onClick={() => setState("m")}>
+            Relatorio
+            <br />
+            de manutenção
+          </button>
+          <button onClick={() => setState("s")}>
+            Especificações
+            <br />
+            de equipamento
+          </button>
+          <button className="disab" disabled onClick={() => setState("o")}>
+            Ordem
+            <br />
+            de compra
+          </button>
         </div>
-    )
-}
+        <div className="form">
+          <h1>Dados para documento:</h1>
+          {state === "w" && <Withdrawal />}
+          {state === "m" && <MainReport />}
+          {state === "s" && <Specifications />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Documents;
--- frontend/src/Pages/Signin/index.tsx
+++ frontend/src/Pages/Signin/index.tsx
@@ -1,68 +1,97 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-import {toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import {useHistory} from 'react-router-dom';
+import { css } from "@emotion/core";
+import BeatLoader from "react-spinners/BeatLoader";
 
-import './style.css';
+import { useHistory } from "react-router-dom";
 
-toast.configure(
-    
-);
+import "./style.css";
+
+toast.configure();
 const Signin = () => {
-    const [username, setUsername] = useState('');
-    const [password,setPassword] = useState('');
-    const history = useHistory();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [state, setState] = useState(false);
+  const history = useHistory();
 
-    const handleSubmit = (e:any) =>{
-        e.preventDefault();
+  const override = css`
+    display: block;
+    margin: 0 auto;
+  `;
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setState(true);
 
-        if(username === 'admin' && password === 'admin'){
-            history.push('/home')
-            toast.success(`Seja bem vindo ${username}`)
-        }
-        else{
-            toast.error('Usuario ou senha incorretos',{
-                position: toast.POSITION.TOP_RIGHT,
-                autoClose: 8000
-            });
-        }
-    }
-return(
+    setTimeout(() => {
+      setState(false);
+      if (username === "admin" && password === "admin") {
+        history.push("/home");
+
+        toast.success(`Seja bem vindo ${username}`);
+      } else {
+        toast.error("Usuario ou senha incorretos", {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 8000,
+        });
+      }
+    }, 2000);
+  };
+  return (
     <div id="page-signin">
-        <section className="logo-bar">
-            <h1>OS Manager</h1>
-            <p>Sistema de gerenciamento de chamados para melhoria no atendimento
-                 da Secretaria de Comunicação e tecnologia.</p>
-        </section>
-        <div className="content"> 
-            <h1>Acesse sua conta!</h1>
+      <section className="logo-bar">
+        <h1>OS Manager</h1>
+        <p>
+          Sistema de gerenciamento de chamados para melhoria no atendimento da
+          Secretaria de Comunicação e tecnologia.
+        </p>
+      </section>
+      <div className="content">
+        <h1>Acesse sua conta!</h1>
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">Nome de usuario: *</label>
-                <input
-                type="text"
-                placeholder="Digite seu nome de usuario:"
-                value={username}
-                onChange={e => setUsername(e.target.value)}/>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="">Nome de usuario: *</label>
+          <input
+            type="text"
+            placeholder="Digite seu nome de usuario:"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-                <label htmlFor="">Senha: *</label>
-                <input 
-                type="password" 
-                placeholder="Digite sua senha:"
-                value={password}
-                onChange={e => setPassword(e.target.value)}/>
+          <label htmlFor="">Senha: *</label>
+          <input
+            type="password"
+            placeholder="Digite sua senha:"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-                <button type="submit" >ENTRAR</button>
-                <a href="/identify">Esqueceu sua senha?</a>
-                <label className="or"><hr /> <p> ou </p> <hr /></label>
-                <button onClick={() => history.push('/signup')}>REGISTRAR</button>
-            </form>
-            <p className="footer">Created by <strong>Creative Project</strong> size in 2020</p>
-        </div>
+          <button type="submit">
+            {state ? (
+              <BeatLoader
+                css={override}
+                size={20}
+                color={"#FFF"}
+                loading={state}
+              />
+            ) : (
+              <p>Salvar</p>
+            )}
+          </button>
+          <a href="/identify">Esqueceu sua senha?</a>
+          <label className="or">
+            <hr /> <p> ou </p> <hr />
+          </label>
+          <button onClick={() => history.push("/signup")}>REGISTRAR</button>
+        </form>
+        <p className="footer">
+          Created by <strong>Creative Project</strong> size in 2020
+        </p>
+      </div>
     </div>
-)
-}
+  );
+};
 
 export default Signin;
