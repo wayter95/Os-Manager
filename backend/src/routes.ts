e28b3308a176736214a905ce4f6e3306c7c8cc94
Wayter <wayter.paulo.95@gmail.com>
2020-07-31 14:00:45 -0300

    init create controllers

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,18 @@
-import express, { response } from "express";
+import express from "express";
+import Knex from './database/connection';
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
     return res.json({
-        message: "Initialized Api"
+        message: "Initialized Api!"
     });
 });
+
+routes.get('/sectors', async (req, res) => {
+    const sectors = await Knex('sectors').select('*');
+
+    return res.json(sectors);
+})
 
 export default routes;
